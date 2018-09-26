e372a2b43e762ffe4739c989832567477b89dd11
minhhieu <minhhieu1096@gmail.com>
2018-09-26 15:45:13 +0700

    doi tuong prompt

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import { Prompt } from 'react-router-dom';
 
 class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isChecked: false
+    }
+  }
+
+  onClick = (isChecked) => {
+    this.setState({
+      isChecked: isChecked
+    })
+  }
   render() {
+    var { isChecked } = this.state;
     return (
       <div>
-        Đây là trang lien he
-     </div>
+        Đây là trang lien he<br />
+        <button type="button" className="btn btn-info" onClick={() => this.onClick(false)}>Cho phép</button>
+        <button type="button" className="btn btn-default" onClick={() => this.onClick(true)}>Không cho phép</button>
+        <Prompt
+          when={isChecked}
+          message="Are you sure you want to leave?"
+        />
+      </div>
     );
   }
 }
